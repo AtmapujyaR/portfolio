1b094e5b2ef56de55e286abcffe38a84f5747a6f
AtmapujyaR <atmapujyar@gmail.com>
2024-10-04 20:27:55 +0530

    Some minor fix

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { portfolioContent } from '../data/portfolioContent'
 
 export function Skills() {
